ff1e5d0052b82799395b659ec4b3618043ffc36a
captainWaheed <thecaptain0078@gmail.com>
2022-10-25 22:52:36 +0500

    contributors list updated

--- data.js
+++ data.js
@@ -1,29 +1,23 @@
 const contributors = [
   {
-    firstname: 'Abdul',
-    lastname: 'Waheed',
-    Github: 'https://github.com/captainwaheed',
+    firstname: "Abdul",
+    lastname: "Waheed",
+    Github: "https://github.com/captainwaheed",
   },
   {
-    firstname: 'Garin',
-    lastname: 'YR',
-    Github: 'https://github.com/garinyr',
+    firstname: "Garin",
+    lastname: "YR",
+    Github: "https://github.com/garinyr",
   },
   {
-    firstname: 'Muhammad',
-    lastname: 'Ahmad',
-    Github: 'https://github.com/Ahmadexe',
+    firstname: "Muhammad",
+    lastname: "Ahmad",
+    Github: "https://github.com/Ahmadexe",
   },
 
   {
     firstname: "Ayesha",
     lastname: "Amjad",
     GitHub_Url: "https://github.com/Ayesha-Amjad",
-},
-
-
-
-
+  },
 ];
-
-
