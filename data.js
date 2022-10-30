a86fb773aac09ec75d613e9dc83a6bf50f6ea81f
NoorFatima01 <nfatima101204@gmail.com>
2022-10-30 11:08:03 +0500

    Added data

--- data.js
+++ data.js
@@ -40,4 +40,9 @@ const contributors = [
     lastname: "Ganesh",
     GitHub_Url: "https://github.com/ganesh1729ganesh",
 },
+{
+  firstname: "Noor",
+  lastname: "Fatima",
+  Github: "https://github.com/NoorFatima01",
+},
 ];
